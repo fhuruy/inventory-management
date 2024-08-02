836dcee36c40140b7108f73964a270e91812919e
Feven Huruy <fhuruy@terpmail.umd.edu>
2024-08-02 16:09:43 -0400

    added conditional for analytics in firebase.js, might be causing deployment issues

--- firebase.js
+++ firebase.js
@@ -19,7 +19,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 const firestore = getFirestore(app)
+let analytics;
+if (typeof window !== 'undefined') {
+  analytics = getAnalyticsBrowser(app);
+}
 
-export {firestore}
+export {firestore, analytics}
